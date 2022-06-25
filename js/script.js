3eafd1f9120a4a14b484fb7428e383929777a20e
Hitesh <hites_297@yahoo.co.in>
2022-06-25 05:51:11 -0400

    experience section complete

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//handle events for right menu
 var hamburger = document.getElementById("hamburger");
 var rightmenu = document.getElementById("right-menu");
 var rightmenuitems = document.querySelectorAll("#right-menu ol a");
@@ -20,3 +21,52 @@ function toggleMenu() {
         closeicon.style.display = 'none';
     }
 }
+
+//hide navbar on scroll
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("header").style.top = "0";
+  } else {
+    document.getElementById("header").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+
+// show tab selector movement in experience section
+const tabs = document.getElementsByClassName('tabs');
+console.log(tabs.length);
+for (var i = 0; i < tabs.length; i++) {
+    tabs[i].addEventListener('click', e => handleClick(e));
+}
+
+function handleClick(e) {
+    console.log("clicked");
+    const target = e.target;
+    const tabNum = target.dataset.tab;
+    const activeTab = document.querySelector('.tabs.active');
+    const activeContent = document.querySelector(
+      '.content__section.visible'
+    );
+    const currentContent = document.querySelector(
+      `.content__section[data-tab='${tabNum}']`
+    );
+    
+    if (!tabNum) {
+      return;
+    }
+    
+    activeTab.classList.remove('active');
+    target.classList.add('active');
+    activeContent.classList.remove('visible');
+    currentContent.classList.add('visible');
+
+
+    var selector = document.getElementById('company-selector');
+    var selectorLocation = (tabNum * 42) - 42;
+    console.log("location:",selectorLocation);
+    selector.style.transform = "translateY("+ selectorLocation + "px)";
+  }
+  
